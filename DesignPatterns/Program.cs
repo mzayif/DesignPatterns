31116917a631750004f3cf76966c44c8b73591a6
Muhammed Zayif <muhammed.zayif@gmail.com>
2024-04-04 02:54:50 +0300

    Add project files.

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatterns;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Singleton class : Nesneden 2 tane farklı değişken üretip 1. nesneye değer verilmektedir. \r\n2 nesneye değer verilmemesine rağmen bilgiler yazdırıldığında aynı değerlere ulaşılmaktadır. ");
+        var singleton = Singleton.NesneVer();
+        singleton.X = 10;
+        singleton.Y = 20;
+
+        var singleton2 = Singleton.NesneVer();
+        Console.WriteLine($"{singleton2.X} + {singleton2.Y} = {singleton2.X + singleton2.Y}");
+
+
+        Console.ReadLine();
+    }
+}
---
+++ DesignPatterns/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DesignPatterns;
+
+public class Singleton
+{
+    // Bir sınıf singleton olabilmesi için default cont erişilmez (private) olmalı.
+    // Böylece sınıfın üretilmesi kontrol altına lınabilir.
+    private Singleton()
+    {
+
+    }
+
+    private static Singleton _singleton;
+
+    //static metodun içinde bu referansı kullanabilmemiz için static olması gerekiyor.
+    //static metodun içinde sadece static metodlar kullanılabilir.
+    public static Singleton NesneVer()
+    {
+        if (_singleton == null)
+        {
+            _singleton = new Singleton();
+        }
+
+        return _singleton;
+    }
+
+    public int X;
+    public int Y;
+}
